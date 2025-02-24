f23be394e6da76ddb03cf3e45cf6b551d9490c44
Ludvig Malm <mail@gmail.com>
2025-02-24 08:17:06 +0100

    Fixed password variable not being inside connect

--- PDO SQL demo code/users.php
+++ PDO SQL demo code/users.php
@@ -7,7 +7,7 @@ $password = "";
 $dbname = "your_database";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     $query = "SELECT * FROM users";
