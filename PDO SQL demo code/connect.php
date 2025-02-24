69bb0470ebbdddfdb3e14349ee05320957f6ab9d
Ludvig Malm <mail@gmail.com>
2025-02-24 16:57:30 +0100

    Removed comment on connection code

--- PDO SQL demo code/connect.php
+++ PDO SQL demo code/connect.php
@@ -9,7 +9,6 @@ $dbname = "your_database";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch(PDOException $e) {
